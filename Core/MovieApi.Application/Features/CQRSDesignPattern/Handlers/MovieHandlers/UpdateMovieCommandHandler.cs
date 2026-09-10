b77d071dd9434ccb984a47698804d368cc12e1aa
isakcmz <isafurkankacmaz@gmail.com>
2026-09-10 15:07:38 +0300

    Category ve Movie için Create, Remove ve Update Command Handler'ları yazıldı.

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/UpdateMovieCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class UpdateMovieCommandHandler
+    {
+        private readonly MovieContext _context;
+        public UpdateMovieCommandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+
+        public async void Handle(UpdateMovieCommand command)
+        {
+            var value = await _context.Movies.FindAsync(command.MovieId);
+            value.Rating = command.Rating;
+            value.Title = command.Title;
+            value.CoverImageUrl = command.CoverImageUrl;
+            value.Description = command.Description;
+            value.Duration = command.Duration;
+            value.ReleaseDate = command.ReleaseDate;
+            value.CreatedYear = command.CreatedYear;
+            value.Status = command.Status;
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}

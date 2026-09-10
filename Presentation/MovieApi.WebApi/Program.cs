46ac1736950b595bc3ee20cf3c27db76ae76d71c
isakcmz <isafurkankacmaz@gmail.com>
2026-09-10 17:05:11 +0300

    Movies için controller oluşturma ve program.cs'ye registiration işlemi

--- Presentation/MovieApi.WebApi/Program.cs
+++ Presentation/MovieApi.WebApi/Program.cs
@@ -1,8 +1,22 @@
+using MovieApi.Application.Features.CQRSDesignPattern.Handlers.CategoryHandlers;
+using MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 
+builder.Services.AddScoped<GetCategoryQueryHandler>();
+builder.Services.AddScoped<GetCategoryByIdQueryHandler>();
+builder.Services.AddScoped<CreateCategoryCommandHandler>();
+builder.Services.AddScoped<UpdateCategoryCommandHandler>();
+builder.Services.AddScoped<RemoveCategoryCommandHandler>();
+
+builder.Services.AddScoped<GetMovieQueryHandler>();
+builder.Services.AddScoped<GetMovieByIdQueryHandler>();
+builder.Services.AddScoped<CreateMovieCommandHandler>();
+builder.Services.AddScoped<UpdateMovieCommandHandler>();
+builder.Services.AddScoped<RemoveMovieCommandHandler>();
 
 
 

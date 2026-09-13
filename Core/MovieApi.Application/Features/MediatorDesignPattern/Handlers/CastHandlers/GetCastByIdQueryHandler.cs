afd8c827cb72063d85744858ea08e135481f83b0
isakcmz <isafurkankacmaz@gmail.com>
2026-09-13 13:49:57 +0300

    Cast için MediatR kullanarak Listeleme için olan QueryHandler'ları yazma

--- Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/GetCastByIdQueryHandler.cs
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/GetCastByIdQueryHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Queries.CastQueries;
+using MovieApi.Application.Features.MediatorDesignPattern.Results.CastResults;
+using MovieApi.Persistence.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,28 @@ using System.Threading.Tasks;
 
 namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.CastHandlers
 {
-    internal class GetCastByIdQueryHandler
+    public class GetCastByIdQueryHandler : IRequestHandler<GetCastByIdQuery, GetCastByIdQueryResult>
     {
+        private readonly MovieContext _context;
+
+        public GetCastByIdQueryHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GetCastByIdQueryResult> Handle(GetCastByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _context.Casts.FindAsync(request.CastId);
+            return new GetCastByIdQueryResult
+            {
+                CastId = values.CastId,
+                Title = values.Title,
+                Name = values.Name,
+                Surname = values.Surname,
+                ImageUrl = values.ImageUrl,
+                OverView = values.OverView,
+                Biography = values.Biography
+            };
+        }
     }
 }
--- Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/GetCastQueryHandler.cs
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/GetCastQueryHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MovieApi.Application.Features.MediatorDesignPattern.Queries.CastQueries;
+using MovieApi.Application.Features.MediatorDesignPattern.Results.CastResults;
+using MovieApi.Persistence.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,31 @@ using System.Threading.Tasks;
 
 namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.CastHandlers
 {
-    internal class GetCastQueryHandler
+    public class GetCastQueryHandler : IRequestHandler<GetCastQuery, List<GetCastQueryResult>>
     {
+        private readonly MovieContext _context;
+
+        public GetCastQueryHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<GetCastQueryResult>> Handle(GetCastQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _context.Casts.ToListAsync();
+            return values.Select(x => new GetCastQueryResult
+            {
+                Biography = x.Biography,
+                CastId = x.CastId,
+                ImageUrl = x.ImageUrl,
+                Name = x.Name,
+                OverView = x.OverView,
+                Surname = x.Surname,
+                Title = x.Title
+            }).ToList();
+
+        }
+
+
     }
 }

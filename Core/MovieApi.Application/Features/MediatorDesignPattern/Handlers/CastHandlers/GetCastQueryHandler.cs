b45efb9349181d2e06aaeba00e328b5fb440561d
isakcmz <isafurkankacmaz@gmail.com>
2026-09-13 13:35:55 +0300

    Cast için MedaitR kütüphanesi kullanarak Command Handler'ları oluşturma

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/GetCastQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.CastHandlers
+{
+    internal class GetCastQueryHandler
+    {
+    }
+}
---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/UpdateCastCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Commands.CastCommands;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.CastHandlers
+{
+    public class UpdateCastCommandHandler : IRequestHandler<UpdateCastCommand>
+    {
+        private readonly MovieContext _context;
+
+        public UpdateCastCommandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(UpdateCastCommand request, CancellationToken cancellationToken)
+        {
+            var values = await _context.Casts.FindAsync(request.CastId);
+            values.Name = request.Name;
+            values.Surname = request.Surname;
+            values.Title = request.Title;
+            values.ImageUrl = request.ImageUrl;
+            values.OverView = request.OverView;
+            values.Biography = request.Biography;
+            await _context.SaveChangesAsync();
+        }
+    }
+}

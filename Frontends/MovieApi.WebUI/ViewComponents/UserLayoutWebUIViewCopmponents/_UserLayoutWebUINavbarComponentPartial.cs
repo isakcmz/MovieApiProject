fcee9d9b5068de824baf565939fb74b9c0857a7c
isakcmz <isafurkankacmaz@gmail.com>
2026-09-14 22:45:30 +0300

    Navbar UI düzenlemeleri

---
+++ Frontends/MovieApi.WebUI/ViewComponents/UserLayoutWebUIViewCopmponents/_UserLayoutWebUINavbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.ViewComponents.UserLayoutWebUIViewCopmponents
+{
+    public class _UserLayoutWebUINavbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}

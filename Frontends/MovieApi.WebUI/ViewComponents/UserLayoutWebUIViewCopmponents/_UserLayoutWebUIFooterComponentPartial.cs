cdbd5b6d209d20f8d0fec15ffe816eb4d1540513
isakcmz <isafurkankacmaz@gmail.com>
2026-09-15 12:25:02 +0300

    Film Listesi sayfasının UI düzenlemelerinin tamamlanması

---
+++ Frontends/MovieApi.WebUI/ViewComponents/UserLayoutWebUIViewCopmponents/_UserLayoutWebUIFooterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.ViewComponents.UserLayoutWebUIViewCopmponents
+{
+    public class _UserLayoutWebUIFooterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}

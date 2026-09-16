ab75fa4077b3b001cf745604fbf4dbaecfb2aff8
isakcmz <isafurkankacmaz@gmail.com>
2026-09-16 22:32:25 +0300

    Admin UI için kalan kodları Component Partial'a dönüştürme ve Area kullanma

---
+++ Frontends/MovieApi.WebUI/Areas/Admin/Controllers/MovieController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MovieController : Controller
+    {
+        public IActionResult MovieList()
+        {
+            return View();
+        }
+    }
+}
--- Frontends/MovieApi.WebUI/Program.cs
+++ Frontends/MovieApi.WebUI/Program.cs
@@ -27,5 +27,13 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+        );
+});
+
 
 app.Run();

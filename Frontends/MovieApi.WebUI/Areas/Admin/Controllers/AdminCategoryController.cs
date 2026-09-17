88e23319eb512d9e2b237d8719e65aeb761e4d97
isakcmz <isafurkankacmaz@gmail.com>
2026-09-17 13:40:27 +0300

    Admin sayfasında Kategorilerin Listelenmesi için Dto'nun, Controller'ın, View'in yazılması ve API Consume işlemi

---
+++ Frontends/MovieApi.WebUI/Areas/Admin/Controllers/AdminCategoryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Dto.Dtos.AdminCategoryDtos;
+using Newtonsoft.Json;
+
+namespace MovieApi.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminCategoryController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCategoryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> CategoryList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7047/api/Categories");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<AdminResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+
+
+            return View();
+        }
+    }
+}

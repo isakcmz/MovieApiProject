9cf8ceac8c7075307708bd162207cdb0d0b70ac3
isakcmz <isafurkankacmaz@gmail.com>
2026-09-17 19:05:55 +0300

    Series için CQRS mimarisi ile Command'ların, Query'lerin ve Result'ların yazılması

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/SeriesHandlers/CreateSeriesCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using MovieApi.Application.Features.CQRSDesignPattern.Commands.SeriesCommands;
+using MovieApi.Domain.Entities;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.SeriesHandlers
+{
+    public class CreateSeriesCommandHandler
+    {
+        private readonly MovieContext _context;
+
+        public CreateSeriesCommandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task Handle(CreateSeriesCommand command)
+        {
+            _context.Serieses.Add(new Series
+            {
+                CoverImageUrl = command.CoverImageUrl,
+                CreatedYear = command.CreatedYear,
+                Description = command.Description,
+                Rating = command.Rating,
+                Status = command.Status,
+                Title = command.Title,
+                AverageEpisodeDuration = command.AverageEpisodeDuration,
+                CategoryId = command.CategoryId,
+                EpisodeCount = command.EpisodeCount,
+                FirstAirDate = command.FirstAirDate,
+                SeasonCount = command.SeasonCount,
+            });
+
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Results/SeriesResults/GetSeriesQueryResult.cs
@@ -0,0 +1,27 @@
+﻿using MovieApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Results.SeriesResults
+{
+    public class GetSeriesQueryResult
+    {
+        public int SeriesId { get; set; }
+        public string Title { get; set; }
+        public string CoverImageUrl { get; set; }
+        public decimal Rating { get; set; }
+        public string Description { get; set; }
+        public DateTime FirstAirDate { get; set; }
+        public int CreatedYear { get; set; }
+        public int? AverageEpisodeDuration { get; set; }
+        public int SeasonCount { get; set; }
+        public int EpisodeCount { get; set; }
+        public string Status { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
